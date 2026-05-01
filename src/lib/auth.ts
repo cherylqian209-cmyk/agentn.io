3e4a23b2fa1a1b10092879f2367fbd0848092b05
Cheryl Qian <cherylqian209@gmail.com>
2026-05-01 07:50:59 -0400

    Remove server auth dependency from root page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,14 +3,20 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
 import { prisma } from './prisma'
 
+const googleClientId = process.env.GOOGLE_CLIENT_ID
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
+  providers:
+    googleClientId && googleClientSecret
+      ? [
+          GoogleProvider({
+            clientId: googleClientId,
+            clientSecret: googleClientSecret,
+          }),
+        ]
+      : [],
   pages: {
     signIn: '/login',
   },
